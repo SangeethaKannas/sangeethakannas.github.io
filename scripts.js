1d8b50128c0d5efb27c4883235f3bf0a5c1253e4
Sangeetha Kanna <sangeethakannas@gmail.com>
2021-01-16 18:12:21 +0000

    Moved scripts to separate file

---
+++ scripts.js
@@ -0,0 +1,132 @@
+
+    //Append tabs - In Reverse Order
+    const tabsElement = document.querySelector('.tabs');
+
+    const currentLocation = window.location.href;
+    let tabs = [];
+    if(currentLocation.indexOf('pesto') > -1) {
+      tabs = ['SWOT', 'Questions']
+    } else if(currentLocation.indexOf('me') > -1) {
+      tabs = ['Requirements', 'Ideas', 'OSS', 'Blogs', 'Resources']
+    } else {
+      tabs = [ 'Experience', 'Projects', 'Summary'];
+    }
+
+    tabs.forEach(currentText => {
+        const currentTextLowerCase = currentText.replace(' ','-').toLowerCase();
+        let input = document.createElement("input");
+        input.setAttribute('type', 'radio');
+        input.setAttribute('name', 'tabs');
+        input.setAttribute('id', currentTextLowerCase);
+        input.setAttribute('checked', '');
+
+        let label = document.createElement("label");
+        label.classList.add('tab-label');
+        label.setAttribute('for', currentTextLowerCase);
+        label.innerText = currentText;
+
+        tabsElement.prepend(label);
+        tabsElement.prepend(input);
+      });
+
+    const handleError = (error) => console.error(error);
+
+    const parseData = data => data.json();
+    const requirementsReduceFn = (acc, value) => acc + '<li>' + value + '</li>';
+
+    const parseResourcesFn = data => {
+      const reducerFn = (acc, value) => acc + '<h2>' + value + '</h2>' + data[value].reduce(requirementsReduceFn, '');
+
+      document.getElementById('links')
+        .innerHTML = Object.keys(data).reduce(reducerFn, '')
+    }
+
+    fetch('/readable_links.json')
+      .then(parseData)
+      .then(parseResourcesFn)
+      .catch(handleError);
+
+    const parseQuestionFn = data => {
+      const dataKeys = Object.keys(data);
+      const questionsReduceFn = (acc, value) =>
+        acc + '<li>' +
+            '<span class="question-span">' + value.question + '</span>' +
+            '<span class="answer-span">' + value.answer + '</span>' +
+          '</li>';
+      const reducerFn = (acc, value) => acc + '<h2>' + value + '</h2>' + data[value].reduce(questionsReduceFn, '');
+      document.getElementById('all-questions')
+        .innerHTML = dataKeys.reduce(reducerFn, '')
+    }
+    fetch('/questions.json')
+      .then(parseData)
+      .then(parseQuestionFn)
+      .catch(handleError);
+
+    const parseReqnFn = data => {
+      const reducerFn = (acc, value) => acc + '<h2>' + value + '</h2>' + data[value].reduce(requirementsReduceFn, '');
+
+      document.getElementById('requirements-article')
+        .innerHTML = Object.keys(data).reduce(reducerFn, '')
+    }
+
+    fetch('/requirements.json')
+      .then(parseData)
+      .then(parseReqnFn)
+      .catch(handleError);
+
+    const parseIdeasFn = data => {
+      const reducerFn = (acc, value) => acc + '<h2>' + value + '</h2>' + data[value].reduce(requirementsReduceFn, '');
+
+      document.getElementById('ideas-article')
+        .innerHTML = Object.keys(data).reduce(reducerFn, '')
+    }
+
+    fetch('/ideas.json')
+      .then(parseData)
+      .then(parseIdeasFn)
+      .catch(handleError);
+
+    let myDetails = {};
+    fetch('/me.json')
+      .then(parseData)
+      .then(data => myDetails = data)
+      .then(() => {
+
+        const swotSection = document.querySelector(".swot");
+        const swot = myDetails['SWOT'];
+        const swotKeys = Object.keys(swot);
+        const swotParseFn = (acc, val) => acc +
+            `<article class="${val}">
+              <div>
+                <h3>${Object.keys(swot[val])[0]}</h3>
+                <ul>
+                  ${Object.values(swot[val])[0].reduce(requirementsReduceFn)}
+                </ul>
+              </div>
+              <div>
+                <h3>${Object.keys(swot[val])[1]}</h3>
+                <ul>
+                  ${Object.values(swot[val])[1].reduce(requirementsReduceFn)}
+                </ul>
+              </div>
+            </article>`;
+        swotSection.innerHTML = `<h2>SWOT</h2>${swotKeys.reduce(swotParseFn, '')}`;
+
+        const projectsSection = document.querySelector(".projects");
+        const projects = myDetails['Projects'];
+
+        projectsSection.innerHTML =
+          `<h2>Projects</h2>
+            ${projects.reduce((acc, value) =>
+              ` <h5>
+                  <a href="${value.link}" target="_blank" rel="noreferrer noopener">${value.name}</a>
+                  <span class="duration">${value.duration}
+                </h5>
+                <code>
+                  Environment: ${value.environment}
+                </code>
+              `, '')}
+
+          `
+      })
+      .catch(handleError);
