110bee290929d56fc1a759ff93f75461485b67b6
Sangeetha Kanna S <sangeethakannas@gmail.com>
2023-07-18 18:54:29 +0530

    Fixed some basic styles

--- scripts.js
+++ scripts.js
@@ -1,64 +1,64 @@
 
-    //Append tabs - In Reverse Order
-    const tabsElement = document.querySelector('.tabs');
+//Append tabs - In Reverse Order
+const tabsElement = document.querySelector('.tabs');
 
-    const currentLocation = window.location.href;
-    let tabs = [];
-    if(currentLocation.indexOf('pesto') > -1) {
-      tabs = ['SWOT', 'Questions']
-    } else if(currentLocation.indexOf('me') > -1) {
-      tabs = ['Requirements', 'Ideas', 'OSS', 'Blogs', 'Resources']
-    } else {
-      tabs = ['Extras', 'Experience', 'Projects', 'Skills', 'About Me'];
-    }
+const currentLocation = window.location.href;
+let tabs = [];
+if (currentLocation.indexOf('pesto') > -1) {
+  tabs = ['SWOT', 'Questions']
+} else if (currentLocation.indexOf('me') > -1) {
+  tabs = ['Requirements', 'Ideas', 'OSS', 'Blogs', 'Resources']
+} else {
+  tabs = ['Extras', 'Experience', 'Projects', 'Skills', 'About Me'];
+}
 
-    tabs.forEach(currentText => {
-        const currentTextLowerCase = currentText.replace(' ','-').toLowerCase();
-        let input = document.createElement("input");
-        input.setAttribute('type', 'radio');
-        input.setAttribute('name', 'tabs');
-        input.setAttribute('id', currentTextLowerCase);
-        input.setAttribute('checked', '');
+tabs.forEach(currentText => {
+  const currentTextLowerCase = currentText.replace(' ', '-').toLowerCase();
+  let input = document.createElement("input");
+  input.setAttribute('type', 'radio');
+  input.setAttribute('name', 'tabs');
+  input.setAttribute('id', currentTextLowerCase);
+  input.setAttribute('checked', '');
 
-        let label = document.createElement("label");
-        label.classList.add('tab-label');
-        label.setAttribute('for', currentTextLowerCase);
-        label.innerText = currentText;
+  let label = document.createElement("label");
+  label.classList.add('tab-label');
+  label.setAttribute('for', currentTextLowerCase);
+  label.innerText = currentText;
 
-        tabsElement.prepend(label);
-        tabsElement.prepend(input);
-      });
+  tabsElement.prepend(label);
+  tabsElement.prepend(input);
+});
 
-    const handleError = error => console.error(error);
-    const parseResponse = response => response.json();
-    const requirementsReduceFn = (acc, value) => `${acc}<li>${value}</li>`;
+const handleError = error => console.error(error);
+const parseResponse = response => response.json();
+const requirementsReduceFn = (acc, value) => `${acc}<li>${value}</li>`;
 
-    //Parse the resources and links
-    const parseGeneralFn = data => {
-      const reducerFn = (acc, value) => `${acc}<h2>${value}</h2>` + data[value].reduce(requirementsReduceFn, '');
+//Parse the resources and links
+const parseGeneralFn = data => {
+  const reducerFn = (acc, value) => `${acc}<h2>${value}</h2>` + data[value].reduce(requirementsReduceFn, '');
 
-      document.getElementById('links')
-        .innerHTML = Object.keys(data).reduce(reducerFn, '')
-    }
+  document.getElementById('links')
+    .innerHTML = Object.keys(data).reduce(reducerFn, '')
+}
 
-    fetch('/assets/data/readable_links.json')
-      .then(parseResponse)
-      .then(parseGeneralFn)
-      .catch(handleError);
+fetch('/assets/data/readable_links.json')
+  .then(parseResponse)
+  .then(parseGeneralFn)
+  .catch(handleError);
 
-    const expandListItem = (event) => {
-      // console.log(event);
-      // const _target = event.currentTarget;
+const expandListItem = (event) => {
+  // console.log(event);
+  // const _target = event.currentTarget;
 
-      // Array.from(_target.parentElement.children).forEach(element => {
-      //   element.classList.add('is-closed');
-      // });
-       _target.classList.toggle('is-closed');
-    }
-    const parseQuestionFn = data => {
-      const questionsReduceFn = (acc, value) =>
-        acc +
-          `<li class="question-list-item is-closed" onclick="expandListItem(event)">
+  // Array.from(_target.parentElement.children).forEach(element => {
+  //   element.classList.add('is-closed');
+  // });
+  _target.classList.toggle('is-closed');
+}
+const parseQuestionFn = data => {
+  const questionsReduceFn = (acc, value) =>
+    acc +
+    `<li class="question-list-item is-closed" onclick="expandListItem(event)">
             <div class="question-answer">
               <div class="question-span">
                 <h4>${value.question}
@@ -71,51 +71,51 @@
               <span class="answer-span">${value.answer}</span>
             </div>
           </li>`;
-      const reducerFn = (acc, value) => `${acc}<h2>${value}</h2>` + data[value].reduce(questionsReduceFn, '');
-      document.getElementById('all-questions')
-        .innerHTML = Object.keys(data).reduce(reducerFn, '')
-    }
+  const reducerFn = (acc, value) => `${acc}<h2>${value}</h2>` + data[value].reduce(questionsReduceFn, '');
+  document.getElementById('all-questions')
+    .innerHTML = Object.keys(data).reduce(reducerFn, '')
+}
 
-    fetch('/assets/data/questions.json')
-      .then(parseResponse)
-      .then(parseQuestionFn)
-      .catch(handleError);
+fetch('/assets/data/questions.json')
+  .then(parseResponse)
+  .then(parseQuestionFn)
+  .catch(handleError);
 
-    const parseReqnFn = data => {
-      const reducerFn = (acc, value) => `${acc}<h2>${value}</h2>` + data[value].reduce(requirementsReduceFn, '');
+const parseReqnFn = data => {
+  const reducerFn = (acc, value) => `${acc}<h2>${value}</h2>` + data[value].reduce(requirementsReduceFn, '');
 
-      document.getElementById('requirements-article')
-        .innerHTML = Object.keys(data).reduce(reducerFn, '')
-    }
+  document.getElementById('requirements-article')
+    .innerHTML = Object.keys(data).reduce(reducerFn, '')
+}
 
-    fetch('/assets/data/requirements.json')
-      .then(parseResponse)
-      .then(parseReqnFn)
-      .catch(handleError);
+fetch('/assets/data/requirements.json')
+  .then(parseResponse)
+  .then(parseReqnFn)
+  .catch(handleError);
 
-    const parseIdeasFn = data => {
-      const reducerFn = (acc, value) => `${acc}<h2>${value}</h2>${data[value].reduce(requirementsReduceFn, '')}`;
+const parseIdeasFn = data => {
+  const reducerFn = (acc, value) => `${acc}<h2>${value}</h2>${data[value].reduce(requirementsReduceFn, '')}`;
 
-      document.getElementById('ideas-article')
-        .innerHTML = Object.keys(data).reduce(reducerFn, '')
-    }
+  document.getElementById('ideas-article')
+    .innerHTML = Object.keys(data).reduce(reducerFn, '')
+}
 
-    fetch('/assets/data/ideas.json')
-      .then(parseResponse)
-      .then(parseIdeasFn)
-      .catch(handleError);
+fetch('/assets/data/ideas.json')
+  .then(parseResponse)
+  .then(parseIdeasFn)
+  .catch(handleError);
 
-    let myDetails = {};
-    fetch('/assets/data/me.json')
-      .then(parseResponse)
-      .then(data => myDetails = data)
-      .then(() => {
+let myDetails = {};
+fetch('/assets/data/me.json')
+  .then(parseResponse)
+  .then(data => myDetails = data)
+  .then(() => {
 
-        const swotSection = document.querySelector(".swot");
-        const swot = myDetails['SWOT'];
-        const swotKeys = Object.keys(swot);
-        const swotParseFn = (acc, val) => 
-            `${acc}<article class="${val}">
+    const swotSection = document.querySelector(".swot");
+    const swot = myDetails['SWOT'];
+    const swotKeys = Object.keys(swot);
+    const swotParseFn = (acc, val) =>
+      `${acc}<article class="${val}">
               <div>
                 <h3>${Object.keys(swot[val])[0]}</h3>
                 <ul>
@@ -129,14 +129,14 @@
                 </ul>
               </div>
             </article>`;
-        swotSection.innerHTML = `<h2>SWOT</h2>${swotKeys.reduce(swotParseFn, '')}`;
+    swotSection.innerHTML = `<h2>SWOT</h2>${swotKeys.reduce(swotParseFn, '')}`;
 
-        const projectsSection = document.querySelector(".projects");
-        const projects = myDetails['Projects'];
-        projectsSection.innerHTML =
-          `<h2>Projects</h2>
+    const projectsSection = document.querySelector(".projects");
+    const projects = myDetails['Projects'];
+    projectsSection.innerHTML =
+      `<h2>Projects</h2>
             ${projects.reduce((acc, value) => acc +
-              ` <h5>
+        ` <h5>
                   <a href="${value.link}" target="_blank" rel="noreferrer noopener">${value.name}</a>
                   <span class="duration">${value.duration}
                 </h5>
@@ -145,34 +145,32 @@
                 </code>
               `, '')}`;
 
-        // const careerHighlightSection = document.querySelector(".career-highlight");
-        // careerHighlightSection.innerHTML =
-        //   `<h2>Career Highlight</h2>
-        //     <ul>
-        //       ${myDetails['Career Highlight'].reduce(requirementsReduceFn, '')}
-        //     </ul>
-        //   `
-        const skillsReduceFn = (acc, value) => acc +
-            `<code>
+    // const careerHighlightSection = document.querySelector(".career-highlight");
+    // careerHighlightSection.innerHTML =
+    //   `<h2>Career Highlight</h2>
+    //     <ul>
+    //       ${myDetails['Career Highlight'].reduce(requirementsReduceFn, '')}
+    //     </ul>
+    //   `
+    const skillsReduceFn = (acc, value) => acc +
+      `<code>
                 <a href="${value.link}" title="${value.name}">${value.image}</a>
              </code>`
 
-        const skillsSection = document.querySelector(".technical-skills");
-        const skills = myDetails['Skills'];
-        skillsSection.innerHTML =
-          `${
-            Object.keys(skills)
-              .reduce((acc, value) => acc + `<p>${skills[value].reduce(skillsReduceFn,'')}</p>` , '')
-            }
+    const skillsSection = document.querySelector(".technical-skills");
+    const skills = myDetails['Skills'];
+    skillsSection.innerHTML =
+      `${Object.keys(skills)
+        .reduce((acc, value) => acc + `<p>${skills[value].reduce(skillsReduceFn, '')}</p>`, '')
+      }
           `
-        const experience = myDetails['Experience'];
-        const simpleViewSection = document.querySelector('.simple-view');
-        simpleViewSection.innerHTML =
-          `
-            ${
-              Object.keys(experience)
-                .reduce((acc, value) => 
-                  `${acc}
+    const experience = myDetails['Experience'];
+    const simpleViewSection = document.querySelector('.simple-view');
+    simpleViewSection.innerHTML =
+      `
+            ${Object.keys(experience)
+        .reduce((acc, value) =>
+          `${acc}
                   <div class="organization">
                     <div class="job-details">
                       <div>
@@ -193,14 +191,14 @@
                     </div>
                   </div>
               `, '')
-            }
+      }
           `
 
-        const contacts = myDetails['Contact'];
-        const contactsSection = document.querySelector('.social-media-list');
-        contactsSection.innerHTML = contacts
-            .reduce((acc, value, index, array) => 
-            `${acc}<li>
+    const contacts = myDetails['Contact'];
+    const contactsSection = document.querySelector('.social-media-list');
+    contactsSection.innerHTML = contacts
+      .reduce((acc, value, index, array) =>
+        `${acc}<li>
                 <a href="${array[index].link}" target="_blank" rel="noopener noreferrer">
                   <i class="fa ${array[index].icon}"></i>
                   ${array[index].name}
@@ -208,5 +206,20 @@
               </li>
             `, '')
 
-      })
-      .catch(handleError);
+  })
+  .catch(handleError);
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
